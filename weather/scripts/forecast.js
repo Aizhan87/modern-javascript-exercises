a4669d0eb5f3aca1368566a3edabc40125b3869e
Aizhan87 <moonsoul87@hotmail.com>
2019-10-09 17:41:06 -0700

    weather app

---
+++ weather/scripts/forecast.js
@@ -0,0 +1,22 @@
+const key = 'oPkUxce0opZ2wV1WwOENtogB0JuG3NI0'
+
+
+// get city information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
+
+
+// get weather information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+}
